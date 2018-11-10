db90846d2f150680e2ded2eac587b5bd3dd613f7
Raspbian <raspbian@gmx.at>
2018-11-10 19:14:53 +0100

    0% flash?

--- src/Hacks/Misc.cpp
+++ src/Hacks/Misc.cpp
@@ -1,25 +1,28 @@
 #include "Misc.hpp"
 #include "../SDK/EntityList.hpp"
+#include "../SDK/Engine.hpp"
 
 void Misc::NoFlash::Run() {
-    static float maxFlashAmount = 70.0f;
+    static float maxFlashAmount = 700.0f;
     static float writeAmount = maxFlashAmount;
     float flash;
 
     if (!enabled)
         return;
 
-    csgo.ReadBuffer(localPlayer.entityPtr + 0xAC0C, &flash, sizeof(float)); // this is some sort of "time" that goes from 255 to 0 when flashed, i dont really know
+    /*sgo.ReadBuffer(localPlayer.entityPtr + 0xAC0C, &flash, sizeof(float)); // this is some sort of "time" that goes from 255 to 0 when flashed, i dont really know
 
     if (flash <= 0.1f) {
         return;
     }
-
+    flash -= 1.0f;
     writeAmount = maxFlashAmount;
     if (flash <= maxFlashAmount) {
         writeAmount = flash;
     }
-
+    localPlayer = entityList.GetEntityInfo(engine.GetLocalPlayer());
+    csgo.WriteBuffer(localPlayer.entityPtr + 0xAC08, &writeAmount, sizeof(float));
+    csgo.WriteBuffer(localPlayer.entityPtr + 0xAC0C, &writeAmount, sizeof(float));*/
     csgo.WriteBuffer(localPlayer.entityPtr + Offsets.localPlayer.flash, &writeAmount, sizeof(float));
 }
 
